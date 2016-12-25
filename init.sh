5eb82a956180464551fccb14eb8c9620fc4aeae5
Chikahiro Tokoro <uzukifirst@gmail.com>
2016-12-25 17:13:28 +0900

    remove home-brew/cask, it became default including

--- init.sh
+++ init.sh
@@ -24,7 +24,6 @@ if ask 'Homebrew install?'; then
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
   brew doctor
-  brew install caskroom/cask/brew-cask
 fi
 
 if ask 'Homebrew Brewdler install?'; then
